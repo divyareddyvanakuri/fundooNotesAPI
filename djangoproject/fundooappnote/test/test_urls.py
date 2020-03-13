b06736c758f981d20253143e6c38785adda0a99c
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-03-13 15:16:27 +0530

    [Divya]refactor:implemented the test cases for login,logout,forgotpassword and resetpassword in test_urls.py file

--- djangoproject/fundooappnote/test/test_urls.py
+++ djangoproject/fundooappnote/test/test_urls.py
@@ -1,10 +1,26 @@
-import sys
 from django.test import TestCase
-from django.urls import reverse,resolve
-sys.path.append('/home/user/Desktop/fundooappnotes/djangoproject/')
-from fundooappnote.views import CreateUser
+from django.urls import reverse, resolve
+from fundooappnote.views import CreateUser, LoginUser,logout,ForgotPassword,ResetPassword
+
+
 # Create your tests here.
 class TestUrls(TestCase):
     def test_register_url_resolves(self):
         url = reverse('register')
-        self.assertEquals(resolve(url).func.view_class,CreateUser)
+        self.assertEquals(resolve(url).func.view_class, CreateUser)
+
+    def test_login_url_resolves(self):
+        url = reverse('login')
+        self.assertEquals(resolve(url).func.view_class, LoginUser)
+    
+    def test_logout_url_resolves(self):
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func, logout)
+
+    def test_forgotpassword_url_resolves(self):
+        url = reverse('forgotpassword')
+        self.assertEquals(resolve(url).func.view_class,ForgotPassword)
+
+    def test_resetpassword_url_resolves(self):
+        url = reverse('resetpassword')
+        self.assertEquals(resolve(url).func.view_class,ResetPassword)
